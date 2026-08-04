ddb43c135ea0fa66e1a790d22e6a9bd29ed90074
Ashutosh Singh <dev.ashutoshsingh03@gmail.com>
2026-08-04 10:59:29 +0530

    Removed Location Attribute

--- Student Management System/src/com/model/Department.java
+++ Student Management System/src/com/model/Department.java
@@ -1,13 +1,12 @@
 package com.model;
 
 //Record Class
-public record Department(int departmentId, String departmentName, String hodName, String location) {
+public record Department(int departmentId, String departmentName, String hodName) {
 
     @Override
     public String toString() {
         return "Department ID : " + departmentId +
                 "\nDepartment Name : " + departmentName +
-                "\nHOD : " + hodName +
-                "\nLocation : " + location;
+                "\nHOD : " + hodName;
     }
 }
