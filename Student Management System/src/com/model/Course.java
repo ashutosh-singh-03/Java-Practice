c05883b54fdc44054a1ff5ca548a68386fd98e8c
Ashutosh Singh <dev.ashutoshsingh03@gmail.com>
2026-08-04 17:23:13 +0530

    Updated Course Class
    Added Constructors, Getters and Setters
    
    Added toString()

--- Student Management System/src/com/model/Course.java
+++ Student Management System/src/com/model/Course.java
@@ -1,11 +1,78 @@
 package com.model;
 
 public class Course {
-    private String courseId;
-    private Course courseName;
-    private String credits;
+    private final String courseId;
+    private String courseName;
     private Department department;
     private Faculty faculty;
-    private String semester;
+    private int semester;
     private int maximumSeats;
+
+    public Course(
+            String courseId, String courseName, Department department, Faculty faculty, int semester,
+            int maximumSeats
+    ) {
+        this.courseId = courseId;
+        this.courseName = courseName;
+        this.department = department;
+        this.faculty = faculty;
+        this.semester = semester;
+        this.maximumSeats = maximumSeats;
+    }
+
+    public String getCourseId() {
+        return courseId;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public Faculty getFaculty() {
+        return faculty;
+    }
+
+    public void setFaculty(Faculty faculty) {
+        this.faculty = faculty;
+    }
+
+    public int getSemester() {
+        return semester;
+    }
+
+    public void setSemester(int semester) {
+        this.semester = semester;
+    }
+
+    public int getMaximumSeats() {
+        return maximumSeats;
+    }
+
+    public void setMaximumSeats(int maximumSeats) {
+        this.maximumSeats = maximumSeats;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "courseId='" + courseId + '\'' +
+                ", courseName=" + courseName +
+                ", department=" + department.departmentName() +
+                ", faculty=" + faculty.getName() +
+                ", semester='" + semester + '\'' +
+                ", maximumSeats=" + maximumSeats +
+                '}';
+    }
 }
