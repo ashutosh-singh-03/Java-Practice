3b7a0dec2a4bc57cf2322f88531d6ca205a10434
Ashutosh Singh <dev.ashutoshsingh03@gmail.com>
2026-08-05 17:23:55 +0530

    Enrollment Class to handle enrollments

--- Student Management System/src/com/model/Enrollment.java
+++ Student Management System/src/com/model/Enrollment.java
@@ -1,10 +1,57 @@
 package com.model;
 
-import java.util.Date;
+import java.time.LocalDate;
 
 public class Enrollment {
-    private String enrollmentId;
+    private final String enrollmentId;
     private Student student;
     private Course course;
-    private Date enrollmentDate;
+    private LocalDate enrollmentDate;
+
+    public Enrollment(
+            String enrollmentId, Student student, Course course, LocalDate enrollmentDate
+    ) {
+        this.enrollmentId = enrollmentId;
+        this.student = student;
+        this.course = course;
+        this.enrollmentDate = enrollmentDate;
+    }
+
+    public String getEnrollmentId() {
+        return enrollmentId;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public LocalDate getEnrollmentDate() {
+        return enrollmentDate;
+    }
+
+    public void setEnrollmentDate(LocalDate enrollmentDate) {
+        this.enrollmentDate = enrollmentDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Enrollment{" +
+                "enrollmentId='" + enrollmentId + '\'' +
+                ", student=" + student +
+                ", course=" + course +
+                ", enrollmentDate=" + enrollmentDate +
+                '}';
+    }
 }
