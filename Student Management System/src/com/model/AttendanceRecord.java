d721d87f08f86a3ef16c58447422f8c5a2211bfe
Ashutosh Singh <dev.ashutoshsingh03@gmail.com>
2026-08-05 18:52:34 +0530

    Added AttendanceRecord Class to handle attendance

--- Student Management System/src/com/model/AttendanceRecord.java
+++ Student Management System/src/com/model/AttendanceRecord.java
@@ -1,11 +1,70 @@
 package com.model;
 
-import java.util.Date;
+import com.enums.Attendance;
+
+import java.time.LocalDate;
 
 public class AttendanceRecord {
-    private String attendanceId;
+    private final String attendanceId;
     private Student student;
     private Course course;
-    private Date date;
-    private String status;
+    private LocalDate date;
+    private Attendance status;
+
+    public AttendanceRecord(
+            String attendanceId, Student student, Course course, LocalDate date, Attendance status
+    ) {
+        this.attendanceId = attendanceId;
+        this.student = student;
+        this.course = course;
+        this.date = date;
+        this.status = status;
+    }
+
+    public String getAttendanceId() {
+        return attendanceId;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Attendance getStatus() {
+        return status;
+    }
+
+    public void setStatus(Attendance status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "AttendanceRecord{" +
+                "attendanceId='" + attendanceId + '\'' +
+                ", student=" + student +
+                ", course=" + course +
+                ", date=" + date +
+                ", status=" + status +
+                '}';
+    }
 }
